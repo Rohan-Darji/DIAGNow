8833a9c6a360fc1efcbe07778d9123d4e0fb8d1c
Prajwal Jaiswal <prajwalmjaiswal@gmail.com>
2022-04-10 03:50:55 +0530

    Integrate server with the UI

--- medicalstore/src/App.jsx
+++ medicalstore/src/App.jsx
@@ -1,21 +1,30 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
 
-import NavigationBar from "../src/components/navbar/navbar"
-import PatientID from "../src/components/patientID/PatientID"
-import Table from "../src/components/datatable/datatable"
-function App(){
+import NavigationBar from "../src/components/navbar/navbar";
+import PatientID from "../src/components/patientID/PatientID";
+import Table from "../src/components/datatable/datatable";
+
+function App() {
+  const [prescription, setPrescription] = useState({});
   return (
-      <div>
-        <NavigationBar/>
-        <Router>
-          <Routes>
-            <Route path="/" element={<PatientID />} ></Route>
-            <Route path="/getPrescription" element={<Table />} ></Route>
-          </Routes>
-        </Router>
-        {/* <PatientID/>
+    <div>
+      <NavigationBar />
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<PatientID prescription={prescription} setPrescription={setPrescription} />}
+          ></Route>
+          <Route
+            path="/getPrescription"
+            element={<Table prescription={prescription} />}
+          ></Route>
+        </Routes>
+      </Router>
+      {/* <PatientID/>
         <Table /> */}
-      </div>
+    </div>
   );
 }
 export default App;
--- medicalstore/src/components/datatable/datatable.jsx
+++ medicalstore/src/components/datatable/datatable.jsx
@@ -1,73 +1,123 @@
-import "./datatable.css"
+import "./datatable.css";
 
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import * as React from "react";
+import {useState} from "react"
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import axios from "axios";
 
-function createData(
-  Medicine: string,
-  Days: number,
-  DosagesPerDay: number,      
-) {
-  return { Medicine, Days, DosagesPerDay};
-}
+// function createData(Medicine: string, Days: number, DosagesPerDay: number) {
+//   return { Medicine, Days, DosagesPerDay };
+// }
 
-const rows = [
-  createData('Paracetamol', 10, 6),
-  createData('Gontazol', 10, 9),
-  createData('Gabatron', 12, 2),
-  createData('Combiflam', 23,1),    
-  createData('Jamalghota', 5,2),
-];
+// const rows = [
+//   createData("Paracetamol", 10, 6),
+//   createData("Gontazol", 10, 9),
+//   createData("Gabatron", 12, 2),
+//   createData("Combiflam", 23, 1),
+//   createData("Jamalghota", 5, 2),
+// ];
 
-export default function BasicTable() {
-var name = "Jaydeep Jethwa";
-var age = 25;
+export default function BasicTable({ prescription }) {
+  const id = prescription._id;
+  const name = prescription.patientName;
+  const age = prescription.patientAge;
+  const medicineName1 = prescription.medicineName1;
+  const medicineName2 = prescription.medicineName2;
+  const medicineName3 = prescription.medicineName3;
+  const medicineDose1 = prescription.medicineDose1;
+  const medicineDose2 = prescription.medicineDose2;
+  const medicineDose3 = prescription.medicineDose3;
+  const medicineStrength1 = prescription.medicineStrength1;
+  const medicineStrength2 = prescription.medicineStrength2;
+  const medicineStrength3 = prescription.medicineStrength3;
+  const medicineDuration1 = prescription.medicineDuration1;
+  const medicineDuration2 = prescription.medicineDuration2;
+  const medicineDuration3 = prescription.medicineDuration3;
+
+  const [verifyResponse, setVerifyResponse] = useState("")
+
+  const verifyPrescription = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.patch("http://localhost:5000/api/prescriptions/update", {id: id})
+      console.log(response.data)
+      setVerifyResponse("This prescription is now been expired. You cannot use this prescription again!")
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
-      <div className="table">
-<h1>Patient Receipt</h1>
-<div className="details">
-<span> <b>Name</b>  : {name}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-<span> <b>Age</b>  : {age}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+    <div className="table">
+      <h1>Patient Receipt</h1>
+      <div className="details">
+        <span>
+          {" "}
+          <b>Name</b> : {name}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </span>
+        <span>
+          {" "}
+          <b>Age</b> : {age}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </span>
+      </div>
 
-</div>
-<div className="table">
+      <div className="prescriptionOutput">
+        <div className="singleOutput">
+          <h3>Medicine 1: {medicineName1}</h3>
+          <p>
+            <b>Dose:</b> {medicineDose1}
+          </p>
+          <p>
+            <b>Strength:</b> {medicineStrength1}
+          </p>
+          <p>
+            <b>Duration:</b> {medicineDuration1}
+          </p>
+        </div>
 
-    <TableContainer component={Paper} className="maintable">
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead >
-          <TableRow>
-            <TableCell >Medicine</TableCell>
-            <TableCell align="center">Days</TableCell>
-            <TableCell align="center">Dosages/day&nbsp;</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-              <TableRow
-              key={row.Medicine}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-              <TableCell component="th" scope="row">
-                {row.Medicine}
-              </TableCell>
-              <TableCell align="center">{row.Days}</TableCell>
-              <TableCell align="center">{row.DosagesPerDay}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+        {prescription.medicineName2 !== "" && (
+          <div className="singleOutput">
+            <h3>Medicine 2: {medicineName2}</h3>
+            <p>
+              <b>Dose:</b> {medicineDose2}
+            </p>
+            <p>
+              <b>Strength:</b> {medicineStrength2}
+            </p>
+            <p>
+              <b>Duration:</b> {medicineDuration2}
+            </p>
           </div>
-          <div className="btn">
-          <button type="submit" className="button" >Verified</button>
+        )}
+
+        {prescription.medicineName3 !== "" && (
+          <div className="singleOutput">
+            <h3>Medicine 3: {medicineName3}</h3>
+            <p>
+              <b>Dose:</b> {medicineDose3}
+            </p>
+            <p>
+              <b>Strength:</b> {medicineStrength3}
+            </p>
+            <p>
+              <b>Duration:</b> {medicineDuration3}
+            </p>
           </div>
-          </div>
+        )}
+      </div>
+
+      <div className="output">
+        <button type="submit" onClick={verifyPrescription} className="button">
+          Confirm
+        </button>
+        <p className="verifyResponse">{verifyResponse}</p>
+      </div>
+    </div>
   );
 }
