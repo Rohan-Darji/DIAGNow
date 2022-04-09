cbd3c48bba10f28beaf35d51cb13eb04619d60d3
Prajwal Jaiswal <prajwalmjaiswal@gmail.com>
2022-04-10 01:16:28 +0530

    Create Frontend

---
+++ doctors-web-app/src/components/navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./Navbar.css";
+import brand from "./DIAGNow.png";
+import logo from "./iconWithShadow.png";
+import userImg from "./dummy-user-img.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="brand">
+        <img src={logo} alt="brand logo" className="brandLogo" />
+        <img src={brand} alt="brand name" className="brandName" />
+      </div>
+      <div className="loginUser">
+        <h3>Dr. Vinay Patkar</h3>
+        <img src={userImg} alt="user img" className="userImg" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
